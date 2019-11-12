28fda9a4d83b636277153ad0fb425c3d98337d59
daisuke <dsaito85a@gmail.com>
2019-11-12 22:40:08 +0900

    レイアウトをデザインカンプの通り修正

--- layouts/default.vue
+++ layouts/default.vue
@@ -1,12 +1,35 @@
 <template>
   <v-app>
-    <v-app-bar app color="primary">
-      <v-app-bar-nav-icon v-if="isLoggedIn" />
+    <v-navigation-drawer v-model="drawer" app clipped>
+      <v-list>
+        <v-list-item-group>
+          <v-list-item v-for="(item, i) in items" :key="i">
+            <v-list-item-icon>
+              <v-icon v-text="item.icon"></v-icon>
+            </v-list-item-icon>
+            <v-list-item-content>
+              <v-list-item-title v-text="item.text"></v-list-item-title>
+            </v-list-item-content>
+          </v-list-item>
+        </v-list-item-group>
+      </v-list>
+    </v-navigation-drawer>
+    <v-app-bar color="primary" app clipped-left dark>
+      <v-app-bar-nav-icon @click.stop="drawer = !drawer" />
       <v-toolbar-title>Team Maker</v-toolbar-title>
-      <div class="flex-grow-1"></div>
-      <v-toolbar-items v-if="isLoggedIn">
-        <v-btn text>Link 1</v-btn>
-      </v-toolbar-items>
+      <v-spacer></v-spacer>
+      <v-spacer></v-spacer>
+      <div class="d-flex align-center">
+        <v-btn icon>
+          <v-icon>mdi-settings</v-icon>
+        </v-btn>
+        <v-btn icon>
+          <v-icon>mdi-bell</v-icon>
+        </v-btn>
+        <v-btn icon>
+          <v-icon>mdi-account</v-icon>
+        </v-btn>
+      </div>
     </v-app-bar>
     <v-content>
       <nuxt />
@@ -21,6 +44,29 @@ import { LoginStore } from '@/store'
 export default class DefaultLayout extends Vue {
   get isLoggedIn(): boolean {
     return LoginStore.isLoggedIn
+  }
+  data() {
+    return {
+      drawer: true,
+      items: [
+        {
+          icon: 'mdi-home',
+          text: 'ホーム'
+        },
+        {
+          icon: 'mdi-account-search',
+          text: '求人管理'
+        },
+        {
+          icon: 'mdi-account-badge-horizontal',
+          text: '応募者管理'
+        },
+        {
+          icon: 'mdi-share-circle',
+          text: 'その他公報'
+        }
+      ]
+    }
   }
 }
 </script>
