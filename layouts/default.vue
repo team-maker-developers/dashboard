4e39d2c18dedf5275cae29118ded89da31b60de0
daisuke <dsaito85a@gmail.com>
2019-11-12 22:37:07 +0900

    ログイン状態を参照可能とする

--- layouts/default.vue
+++ layouts/default.vue
@@ -1,35 +1,12 @@
 <template>
   <v-app>
-    <v-navigation-drawer v-if="isLoggedIn" v-model="drawer" app clipped>
-      <v-list>
-        <v-list-item-group>
-          <v-list-item v-for="(item, i) in items" :key="i">
-            <v-list-item-icon>
-              <v-icon v-text="item.icon"></v-icon>
-            </v-list-item-icon>
-            <v-list-item-content>
-              <v-list-item-title v-text="item.text"></v-list-item-title>
-            </v-list-item-content>
-          </v-list-item>
-        </v-list-item-group>
-      </v-list>
-    </v-navigation-drawer>
-    <v-app-bar color="primary" app clipped-left dark>
-      <v-app-bar-nav-icon v-if="isLoggedIn" @click.stop="drawer = !drawer" />
+    <v-app-bar app color="primary">
+      <v-app-bar-nav-icon v-if="isLoggedIn" />
       <v-toolbar-title>Team Maker</v-toolbar-title>
-      <v-spacer></v-spacer>
-      <v-spacer></v-spacer>
-      <div v-if="isLoggedIn" class="d-flex align-center">
-        <v-btn icon>
-          <v-icon>mdi-settings</v-icon>
-        </v-btn>
-        <v-btn icon>
-          <v-icon>mdi-bell</v-icon>
-        </v-btn>
-        <v-btn icon>
-          <v-icon>mdi-account</v-icon>
-        </v-btn>
-      </div>
+      <div class="flex-grow-1"></div>
+      <v-toolbar-items v-if="isLoggedIn">
+        <v-btn text>Link 1</v-btn>
+      </v-toolbar-items>
     </v-app-bar>
     <v-content>
       <nuxt />
@@ -39,33 +16,11 @@
 
 <script lang="ts">
 import { Component, Vue } from 'vue-property-decorator'
-import { State } from 'vuex-class'
-
+import { LoginStore } from '@/store'
 @Component
 export default class DefaultLayout extends Vue {
-  @State('isLoggedIn') isLoggedIn: boolean
-  data() {
-    return {
-      drawer: true,
-      items: [
-        {
-          icon: 'mdi-home',
-          text: 'ホーム'
-        },
-        {
-          icon: 'mdi-account-search',
-          text: '求人管理'
-        },
-        {
-          icon: 'mdi-account-badge-horizontal',
-          text: '応募者管理'
-        },
-        {
-          icon: 'mdi-share-circle',
-          text: 'その他公報'
-        }
-      ]
-    }
+  get isLoggedIn(): boolean {
+    return LoginStore.isLoggedIn
   }
 }
 </script>
