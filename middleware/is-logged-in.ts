71073fdd605c1978ac7aeee5781b71e76918f61a
nuno929 <t-mark5@kce.biglobe.ne.jp>
2019-12-20 18:34:17 +0900

    fix: loginStoreをキャメルケースに修正

--- middleware/is-logged-in.ts
+++ middleware/is-logged-in.ts
@@ -1,16 +1,16 @@
 import { Context, Middleware } from '@nuxt/types'
-import { LoginStore } from '@/store'
+import { loginStore } from '@/store'
 const isLoggedIn: Middleware = (context: Context) => {
   const { redirect, route, env } = context
   
   // 本来はアクセストークンを設定せず、モックの時だけisLoggedInの戻り値をTRUEにしたい。
   // しかし、envがisLoggedIn内で参照できないので、暫定的にアクセストークンを設定している
   if (env.isMock) {
-    LoginStore.setAccessToken('dummy_token');
+    loginStore.setAccessToken('dummy_token');
     return
   }
 
-  if (LoginStore.isLoggedIn) {
+  if (loginStore.isLoggedIn) {
     return
   }
 
