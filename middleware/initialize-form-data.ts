6a903efcf307bf4cd2d4328f7e8c634ce1915280
nuno929 <t-mark5@kce.biglobe.ne.jp>
2019-12-21 20:02:54 +0900

    fix : 型安全の強化

--- middleware/initialize-form-data.ts
+++ middleware/initialize-form-data.ts
@@ -1,9 +1,10 @@
 import { Context, Middleware } from '@nuxt/types'
+import { loginStore } from '@/store'
 
-const initializeFormData: Middleware = ({ env, store }: Context) => {
-  store.commit('login/setClientData', {
-    apiClientId: env.apiClientId,
-    apiClientSecret: env.apiClientSecret
+const initializeFormData: Middleware = ({ env }: Context) => {
+  loginStore.setClientData({
+    id: env.apiClientId,
+    secret: env.apiClientSecret
   })
 }
 export default initializeFormData
--- store/login.ts
+++ store/login.ts
@@ -1,66 +1,23 @@
 import axios from 'axios'
 import { Action, Module, Mutation, VuexModule } from 'vuex-module-decorators'
+import { LoginPost, EmailLoginPost, SocialLoginPost } from '~/models/login-post'
+
+interface apiClientData {
+  id: string
+  secret: string
+}
 
 export interface LoginState {
   clientId: string
   accessToken: string
+  apiClientData: apiClientData|null
 }
 
-abstract class BasedLoginPost {
-  url: string
-  apiClientId: string
-  apiClientSecret: string
-
-  get formData(): FormData {
-    const formData = new FormData()
-    formData.append('client_id', this.apiClientId)
-    formData.append('client_secret', this.apiClientSecret)
-    formData.append('scope', '*')
-    return formData
-  }
-
-  set apiClientData({ apiClientId, apiClientSecret }: apiClientData) {
-    this.apiClientId = apiClientId
-    this.apiClientSecret = apiClientSecret
-  }
-
-}
-
-export class EmailLoginPost extends BasedLoginPost {
-  email: string
-  password: string
-
-  get formData(): FormData {
-    const formData = super.formData
-    formData.append('grant_type', 'password')
-    formData.append('username', this.email)
-    formData.append('password', this.password)
-    return formData
-  }
-}
-
-export class SocialLoginPost extends BasedLoginPost {
-  code: string
-
-  get formData(): FormData {
-    const formData = super.formData
-    formData.append('grant_type', 'socialite')
-    formData.append('code', this.code)
-    return formData
-  }
-}
-
-interface apiClientData {
-  apiClientId: string
-  apiClientSecret: string
-}
-  
 @Module({ stateFactory: true, namespaced: true, name: 'login' })
 export default class Login extends VuexModule implements LoginState {
   accessToken: string = ''
   clientId: string = ''
-  apiClientId: string = ''
-  apiClientSecret: string = ''
+  apiClientData: apiClientData|null = null
 
   @Mutation
   setClientId(clientId: string) {
@@ -68,20 +25,25 @@ export default class Login extends VuexModule implements LoginState {
   }
 
   @Mutation
-  setClientData({ apiClientId, apiClientSecret }: apiClientData) {
-    this.apiClientId = apiClientId
-    this.apiClientSecret = apiClientSecret
-  }
-
-  get clientData(): apiClientData {
-    return {
-      apiClientId: this.apiClientId,
-      apiClientSecret: this.apiClientSecret
-    }
+  setClientData(apiClientData: apiClientData) {
+    this.apiClientData = apiClientData
   }
 
   get isLoggedIn(): boolean {
-    return this.accessToken === '' ? false : true;
+    return this.accessToken !== ''
+  }
+
+  get getApiFormData(): (formData: FormData) => FormData {
+    return (formData: FormData) => {
+      if (!this.apiClientData) {
+        throw new Error('環境変数が登録されていません。')
+      }
+  
+      formData.append('client_id', this.apiClientData.id)
+      formData.append('client_secret', this.apiClientData.secret)
+      formData.append('scope', '*')
+      return formData
+    }
   }
 
   @Mutation
@@ -90,22 +52,22 @@ export default class Login extends VuexModule implements LoginState {
   }
 
   @Action
-  async getAccessToken(loginPost: BasedLoginPost) {
-    const response = await axios.post(loginPost.url, loginPost.formData)
+  async getAccessToken(loginPost: LoginPost) {
+    const formData = this.getApiFormData(loginPost.formData)
+    const response = await axios.post(loginPost.url, formData)
     const { data } = response
+
     this.setAccessToken(data.access_token)
     return data.access_token
   }
 
   @Action
   async postEmailLogin(emailLoginPost: EmailLoginPost) {
-    emailLoginPost.apiClientData = this.clientData
     await this.getAccessToken(emailLoginPost)
   }
 
   @Action
   async postSocialLogin(socialLoginPost: SocialLoginPost) {
-    socialLoginPost.apiClientData = this.clientData
     await this.getAccessToken(socialLoginPost)
   }
 }
