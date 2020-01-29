1df22ae55c1ae189ab3bbafadb9075f5fdf2a150
nuno929 <t-mark5@kce.biglobe.ne.jp>
2020-01-30 00:28:34 +0900

    feat : grapQlの外だし

--- constants/list-tables/social-accounts.ts
+++ constants/list-tables/social-accounts.ts
@@ -1,3 +1,5 @@
+import gql from 'graphql-tag'
+
 export const socialAccountHeaders: TableHeader[] = [
   {
     text: 'ID',
@@ -12,3 +14,41 @@ export const socialAccountHeaders: TableHeader[] = [
   { text: 'ステータス', value: 'status' },
   { text: 'アクション', value: 'changeValid' }
 ]
+
+export const getSocialAccountsQuery = gql`
+  query getSocialAccounts {
+    socialAccounts {
+      id
+      image
+      name
+      profile
+      unique_id
+      user {
+        id
+        is_valid
+      }
+      service {
+        name
+      }
+    }
+  }
+`
+
+export const getUpdateUserMutation = (userId: string, changedIsValid: boolean) => {
+  return {
+    mutation: gql`
+    mutation($id: ID!, $isValid: Boolean) {
+      updateUser(id: $id, is_valid: $isValid) {
+        id
+        is_valid
+      }
+    }
+  `,
+  variables: {
+    id: userId,
+    isValid: changedIsValid
+  },
+  refetchQueries: [{ query: getSocialAccountsQuery }]
+
+  }
+}
