a484e2e331eaedbb58aec095fc5ea0c1a6aae764
nuno929 <t-mark5@kce.biglobe.ne.jp>
2020-02-02 13:20:41 +0900

    fix : 定数クラスの修正

--- constants/list-tables/jobs.ts
+++ constants/list-tables/jobs.ts
@@ -1,27 +1,53 @@
+import gql from 'graphql-tag'
+import { jobStore } from '@/store'
+
 export const jobHeaders: TableHeader[] = [
   {
     text: 'No',
     align: 'left',
-    value: 'no'
+    value: 'code'
   },
-  { text: '求人', value: 'job' },
-  { text: '公開ステータス', value: 'status' },
-  { text: '前回シェア日', value: 'share' },
+  { text: '求人', value: 'name' },
+  { text: '公開ステータス', value: 'publishState' },
+  { text: '前回シェア日', value: 'sharedAt' },
   { text: '', value: 'edit' },
   { text: '', value: 'others' }
 ]
 
-export const jobActions: any = [
+export const jobActions: TableAction[] = [
   {
     text: 'LINEでシェア',
-    action: () => null
+    action: (id: number) => {}
   },
   {
-    title: '下書きに戻す',
-    action: () => null 
+    text: '下書きに戻す',
+    alterText: '公開する',
+    action: (id: number) => {}
   },
   {
-    title: '下書きに戻す',
-    action: () => null 
+    text: '削除',
+    action: (id: number) => {
+      jobStore.deleteJob(id)
+    }
  }
 ]
+
+export const getJobs = gql`
+  query getJobs {
+    jobs {
+      id
+      code
+      name
+      publishState
+      isPublished
+    }
+  }
+`
+
+export const deleteJob = gql`
+  mutation($id: ID!) {
+    deleteJob(id: $id) {
+      code
+    }
+  }
+`
