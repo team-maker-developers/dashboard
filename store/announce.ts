6079cce397c3b9e4e1428d91c99f86717b5dfb18
nuno929 <t-mark5@kce.biglobe.ne.jp>
2020-03-28 21:50:27 +0900

    fix : 広報の実処理を追加

---
+++ store/announce.ts
@@ -0,0 +1,75 @@
+import { Action, Mutation, Module, VuexModule } from 'vuex-module-decorators'
+import { getApolloClient } from '@/plugins/apollo/get-apollo-client'
+import { createAnnounce } from '@/constants/models/announce/announce'
+import { rootState } from '@/store'
+
+interface Page {
+  slug: string
+  isPublished: boolean
+  publishState: string
+}
+
+export interface Job {
+  id: string
+  code: string
+  name: string
+  last_announced_at: string
+  page: Page
+}
+
+interface AnnounceState {
+  jobId: string | null
+  job: Job | null
+}
+
+const getCreateAnnounceInput = (announce: string, jobId: string) => ({
+  content: announce,
+  job_id: jobId,
+  channelNotifications: {
+    create: [{
+      channel_id: 1
+    }]
+  }
+})
+
+@Module({ stateFactory: true, namespaced: true, name: 'announce' })
+export default class Announce extends VuexModule implements AnnounceState {
+  job: Job | null = null
+
+  get jobId(): string {
+    return this.job ? this.job.id : ''
+  }
+
+  get apiDomain(): string {
+    return rootState.app.context.env.apiDomain
+  }
+
+  get shareUrl(): string {
+    if (this.job) {
+      return `${this.apiDomain}/${this.job.page.slug}`
+    }
+
+    return ''
+  }
+
+  @Mutation
+  setJob(job: Job) {
+    this.job = job
+  }
+
+  @Mutation
+  clearJob() {
+    this.job = null
+  }
+
+  @Action({ rawError: true })
+  async postAnnounce(announce: string) {
+    await getApolloClient().mutate({
+      mutation: createAnnounce,
+      variables: {
+        input: getCreateAnnounceInput(announce, this.jobId)
+      }
+    })
+    return '広報文の送信に成功いたしました。'
+  }
+}
