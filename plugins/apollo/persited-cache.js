596c3f69d575b9d32e651a69a30c3b970d374556
nuno929 <t-mark5@kce.biglobe.ne.jp>
2020-04-23 15:11:44 +0900

    fix : apolloのキャッシュのコメントアウト

--- plugins/apollo/persited-cache.js
+++ plugins/apollo/persited-cache.js
@@ -1,10 +1,10 @@
-// import { persistCache } from 'apollo-cache-persist'
+import { persistCache } from 'apollo-cache-persist'
 
-// export default async ({ app }) => {
-//   // graphQlの取得結果をキャッシュする
-//   await persistCache({
-//     key: 'tm-apollo',
-//     cache: app.apolloProvider.defaultClient.cache,
-//     storage: window.sessionStorage
-//   })
-// }
+export default async ({ app }) => {
+  // graphQlの取得結果をキャッシュする
+  await persistCache({
+    key: 'tm-apollo',
+    cache: app.apolloProvider.defaultClient.cache,
+    storage: window.sessionStorage
+  })
+}
