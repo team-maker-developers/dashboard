5c6b1e946abec2638fc6f0e874ad0b9b3363b20d
nuno929 <t-mark5@kce.biglobe.ne.jp>
2020-05-14 07:52:55 +0900

    feat : 定数の追加

---
+++ constants/announces/models.ts
@@ -0,0 +1,54 @@
+import { loginStore } from '@/store'
+
+export interface Channel {
+  id: string
+  name: string
+}
+
+interface Page {
+  slug: string
+  isPublished: boolean
+  publishState: string
+}
+
+export interface AnnounceInput {
+  announce: string
+  channels: Channel[]
+}
+
+export interface Job {
+  id: string
+  code: string
+  name: string
+  last_announced_at: string
+  page: Page
+}
+
+export const getCreateAnnounceInput = (
+  announce: string,
+  channels: Channel[],
+  jobId: string
+) => {
+  const channelCreates = channels.map((channel) => ({ channel_id: channel.id }))
+  return {
+    content: announce,
+    job_id: jobId,
+    channelNotifications: {
+      create: channelCreates
+    }
+  }
+}
+
+
+export const getAnnounceUrl = (job: Job) => {
+  if (job) {
+    const domain = window.location.origin
+
+    const reqirectTo = `/share/jobs/${job.id}`
+    return `${domain}/login/?unique_id=${
+      loginStore.uniqueId
+    }&redirect_to=${encodeURIComponent(reqirectTo)}`
+  }
+
+  return ''
+}
