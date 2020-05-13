4debd96d4e1eed0d87ec0fe1e9119e6112d8e750
nuno929 <t-mark5@kce.biglobe.ne.jp>
2020-05-14 07:54:12 +0900

    fix : コンポーネントの分割

---
+++ components/announces/announce-steps/announce-step-header.vue
@@ -0,0 +1,28 @@
+<template>
+  <v-stepper-header>
+    <v-divider></v-divider>
+    <v-stepper-step :complete="currentStep > 1" step="1">
+      広報先を選択する
+    </v-stepper-step>
+    <v-divider></v-divider>
+    <v-stepper-step :complete="currentStep > 2" step="2">
+      広報文を入力する
+    </v-stepper-step>
+    <v-divider></v-divider>
+    <v-stepper-step :complete="currentStep > 3" step="3">
+      入力内容を確認して、LINEで広報する
+    </v-stepper-step>
+    <v-divider></v-divider>
+  </v-stepper-header>
+</template>
+
+<script>
+export default {
+  props: {
+    currentStep: {
+      type: Number,
+      required: true
+    }
+  }
+}
+</script>
