55cc80c0d3f1b8b57e9b264de7bd52329446fee0
nuno929 <t-mark5@kce.biglobe.ne.jp>
2020-06-02 15:56:07 +0900

    feat : 各種環境設定の修正

--- configs/config.staging.js
+++ configs/config.staging.js
@@ -1,4 +1,6 @@
 export const envValues = {
-  apiDomain: 'https://team-maker-app-xkz5u66uea-an.a.run.app',
-  jobBoard: 'https://teammaker-job.web.app/'
+  apiClientId: process.env.STAGE_API_CLIENT_ID || '',
+  apiClientSecret: process.env.STAGE_API_CLIENT_SECRET || '',
+  apiDomain: 'https://team-maker-app-stage-xkz5u66uea-an.a.run.app',
+  jobBoard: 'https://stage-teammaker-job.web.app/'
 }
--- configs/get-config.js
+++ configs/get-config.js
@@ -1,32 +1,15 @@
 require('dotenv').config()
 
-const devSecrets = {
-  apiClientId: process.env.API_CLIENT_ID || '',
-  apiClientSecret: process.env.API_CLIENT_SECRET || ''
-}
-
-const prodSecrets = {
-  apiClientId: process.env.PROD_API_CLIENT_ID || '',
-  apiClientSecret: process.env.PROD_API_CLIENT_SECRET || ''
-}
-
 // 以下がデフォルト値になる
 const baseValues = {
   apiDomain: 'http://localhost:8888',
   jobBoard: 'http://localhost:5000',
-  isMock: false
+  isMock: false,
+  apiClientId: process.env.DEV_API_CLIENT_ID || '',
+  apiClientSecret: process.env.DEV_API_CLIENT_SECRET || ''
 }
 
 export function getConfig(environment) {
   const { envValues } = require(`./config.${environment}.js`)
-
-  const isDev = environment === 'development'
-  const nodeEnvValues = isDev ? devSecrets : prodSecrets
-  return Object.assign(
-    {
-      ...baseValues,
-      ...nodeEnvValues
-    },
-    envValues
-  )
+  return Object.assign({ ...baseValues }, envValues)
 }
