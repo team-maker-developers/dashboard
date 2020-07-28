097478c28eff49f34e1bade7f54b6c276b3d8ab7
nuno929 <t-mark5@kce.biglobe.ne.jp>
2020-07-28 17:55:34 +0900

    feat : ステージング環境の追加

--- configs/config.production.js
+++ configs/config.production.js
@@ -1,6 +1,6 @@
 export const envValues = {
   apiClientId: process.env.PROD_API_CLIENT_ID || '',
   apiClientSecret: process.env.PROD_API_CLIENT_SECRET || '',
-  apiDomain: 'https://team-maker-app-xkz5u66uea-an.a.run.app',
-  jobBoard: 'https://teammaker-job.web.app/'
+  apiDomain: 'https://backend.teammaker.info',
+  jobBoard: 'https://job.teammaker.info'
 }
